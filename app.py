5ed0861774f556a9d86635d9baadf7b7e3cc3b32
Gustavo Martins <gsmartins300@gmail.com>
2024-06-06 10:21:45 -0300

    deploy teste

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 
 st.header("Data App Dismepi")
+st.page_link("./app.py")
 st.page_link("./pages/Dashboard.py")
 st.page_link("./pages/Estoque.py")
 st.page_link("./pages/Grade.py")
