3d3997e416a40a1910e4e621ea61b952fbe12832
Gustavo Martins <gsmartins300@gmail.com>
2024-06-06 10:54:00 -0300

    deploy teste

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import streamlit as st
+from datetime import datetime
+import pytz
 
 st.set_page_config(layout="wide")
 
+
 st.header("Data App Dismepi")
-st.page_link("./app.py")
-st.page_link("./pages/Dashboard.py")
-st.page_link("./pages/Estoque.py")
-st.page_link("./pages/Grade.py")
+data = datetime.now((pytz.timezone('America/Sao_Paulo')))
+
+st.subheader("Atualização - " + str(data.day) +"/"+ str(data.month) +"/"+ str(data.year) +" - "+ str(data.hour) +":"+ str(data.minute) + ":" + str(data.second))
+#st.page_link("./app.py")
+#st.page_link("./pages/Dashboard.py")
+#st.page_link("./pages/Estoque.py")
+#st.page_link("./pages/Grade.py")
