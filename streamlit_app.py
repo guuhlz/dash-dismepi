8f485b71a6e348e070955c07f16189209bea836a
Gustavo Martins <gsmartins300@gmail.com>
2024-06-17 10:14:48 -0300

    data 17061

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,19 @@ import pytz
 
 st.set_page_config(layout="wide")
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 
 st.header("Data App Dismepi")
 data = datetime.now((pytz.timezone('America/Sao_Paulo')))
