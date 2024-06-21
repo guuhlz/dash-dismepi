e3d0edf1670b26281f213ee9749afee2d696c37e
Gustavo Martins <gsmartins300@gmail.com>
2024-06-21 10:32:03 -0300

    data 21061

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.header("Data App Dismepi")
 data = datetime.now((pytz.timezone('America/Sao_Paulo')))
 
 #st.subheader("Atualização - " + str(data.day) +"/"+ str(data.month) +"/"+ str(data.year) +" - "+ str(data.hour) +":"+ str(data.minute) + ":" + str(data.second))
-st.subheader("Atualização - 20/06/2024 - 08:30:00")
+st.subheader("Atualização - 21/06/2024 - 10:31:00")
 #st.page_link("./app.py")
 #st.page_link("./pages/Dashboard.py")
 #st.page_link("./pages/Estoque.py")
